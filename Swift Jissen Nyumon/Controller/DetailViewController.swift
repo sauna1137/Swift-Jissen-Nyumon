0db621881bce8f7f0e927fa6721c3de6a31f5695
sauna <soneko2102@gmail.com>
2021-11-23 13:51:06 +0900

    1st commit

---
+++ Swift Jissen Nyumon/Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  Swift Jissen Nyumon
+//
+//  Created by KS on 2021/11/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
