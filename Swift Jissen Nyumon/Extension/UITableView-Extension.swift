cedf7c3647be971cfa7c66d311ffbac8c808b041
sauna <soneko2102@gmail.com>
2021-11-23 16:14:53 +0900

    1st commit

---
+++ Swift Jissen Nyumon/Extension/UITableView-Extension.swift
@@ -0,0 +1,38 @@
+//
+//  UITableView-Extension.swift
+//  Swift Jissen Nyumon
+//
+//  Created by KS on 2021/11/23.
+//
+
+import Foundation
+import UIKit
+
+
+extension NSObjectProtocol {
+
+    static var className: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell {
+
+    static var identifier: String {
+        return className
+    }
+}
+
+extension UITableView {
+
+    func registerCustomCell<T: UITableViewCell>(_ cellType: T.Type) {
+        register(
+            UINib(nibName: T.identifier, bundle: nil),
+            forCellReuseIdentifier: "Cell"
+        )
+    }
+
+    func dequeueReusableCustomCell<T: UITableViewCell>(with cellType: T.Type) -> T {
+        return dequeueReusableCell(withIdentifier: "Cell") as! T
+    }
+}
