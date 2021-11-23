0d6ff9451931d4fe8794ed0b19c485233bc389e7
sauna <soneko2102@gmail.com>
2021-11-23 22:49:31 +0900

    UILabel追加 遷移時にlabelに勉強した内容を追加

--- Swift Jissen Nyumon/Controller/DetailViewController.swift
+++ Swift Jissen Nyumon/Controller/DetailViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var labelsText = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        label.text = labelsText
     }
 
 }
