cfb0eed2f4379b5acedf4c08d7d062b42bdf30ad
sauna <soneko2102@gmail.com>
2021-11-23 22:51:16 +0900

    配列追加

--- Swift Jissen Nyumon/Model/HomeModel.swift
+++ Swift Jissen Nyumon/Model/HomeModel.swift
@@ -11,6 +11,6 @@ class HomeModel {
 
     static let homeTableViewTitlesArray = ["変数と定数"]
 
-
+    static let DetailVCTextViewsArray = ["let a = 123 \n type(of: a) // Int.Type \n クロージャ { 引数 in 戻り値を返す式 }"]
 
 }
