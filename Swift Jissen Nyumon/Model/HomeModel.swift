3b308090b4cd5548e5144edc4d638d79125955fe
sauna <soneko2102@gmail.com>
2021-11-25 22:55:40 +0900

    微修正

--- Swift Jissen Nyumon/Model/HomeModel.swift
+++ Swift Jissen Nyumon/Model/HomeModel.swift
@@ -11,6 +11,6 @@ class HomeModel {
 
     static let homeTableViewTitlesArray = ["変数と定数と式", "基本的な型"]
 
-    static let DetailVCTextViewsArray = ["let a = 123 \n type(of: a) // Int.Type \n クロージャ { 引数 in 戻り値を返す式 }　\n\n let array = [1,2,3] \n let doubled = original.map({ value in value * 2 })\n doubled // [2,4,6] \n\n let a = 7 -a = -7\n let a = false !a = true \n\n String(4)もStringという関数にint型の4を渡して\"4\"を受け取る"]
+    static let DetailVCTextViewsArray = ["let a = 123 \n type(of: a) // Int.Type \n クロージャ { 引数 in 戻り値を返す式 }　\n\n let array = [1,2,3] \n let doubled = original.map({ value in value * 2 })\n doubled // [2,4,6] \n\n let a = 7 -a = -7\n let a = false !a = true \n\n String(4)もStringという関数にint型の4を渡して\"4\"を受け取る", ""]
 
 }
